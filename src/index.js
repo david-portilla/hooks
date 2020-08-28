a785460cfd4827cc6bb97f7f68b844c26248081c
David Portilla <dportilla@adkgroup.com>
2020-08-28 11:42:12 -0500

    useLayoutEffect

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ import './index.css';
 // import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook'
 // import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks'
 // import FocusScreen from './components/04-useRef/FocusScreen';
-import RealExampleRef from './components/04-useRef/RealExampleRef';
+// import RealExampleRef from './components/04-useRef/RealExampleRef';
+import LayoutEffect from './components/05-useLayoutEffect/LayoutEffect';
 
 ReactDOM.render(
-  <RealExampleRef />,
+  <LayoutEffect />,
   document.getElementById('root')
 );
